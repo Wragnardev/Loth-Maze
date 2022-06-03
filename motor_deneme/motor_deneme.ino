69774651f68c266cfd04beeb6f71e4d6ce301e15
Wragnardev <104067464+Wragnardev@users.noreply.github.com>
2022-06-03 13:45:26 +0300

    push

--- motor_deneme/motor_deneme.ino
+++ motor_deneme/motor_deneme.ino
@@ -1,14 +1,20 @@
 #define ma 8
 #define ca 9
+#define mb 10
+#define cb 11
 
 void setup()
 {
   pinMode(ma, OUTPUT);
   pinMode(ca, OUTPUT);
+  pinMode(mb, OUTPUT);
+  pinMode(cb, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(ma, HIGH);
   digitalWrite(ca, HIGH);
+  digitalWrite(mb, HIGH);
+  digitalWrite(cb, HIGH);
 }
