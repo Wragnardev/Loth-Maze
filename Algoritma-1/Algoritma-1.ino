e1aa91473b9fddf8ec33b17f7c510cb21df9dd8c
Halit Hamza <seker@halithamza.software>
2022-06-08 12:00:43 +0300

    push

--- Algoritma-1/Algoritma-1.ino
+++ Algoritma-1/Algoritma-1.ino
@@ -2,7 +2,7 @@
 int yon=0;
 int x=9;
 int y=0;
-
+//ön 6 sol 5 sağ 4 arka sağ 3 arka sol 2 
 int maze[ 17 ][ 14 ] = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0  },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }};
 
 int leftSensor;
@@ -32,13 +32,13 @@ void back (){
 
 void forward(){
   if(yon==0){
-    y-=1;
-  }else if(yon==1){
-    x+=1;
-  }else if(yon==2){
-    y+=1;
-  }else{
     x-=1;
+  }else if(yon==1){
+    y+=1;
+  }else if(yon==2){
+    x+=1;
+  }else{
+    y-=1;
   }
 }
 
@@ -50,13 +50,36 @@ void forward(){
 
 
 void setup() {
-
+  Serial.begin(9600);
+  pinMode(6, INPUT);
+  pinMode(5, INPUT);
+  pinMode(4, INPUT);
+  pinMode(3, INPUT);
+  pinMode(2, INPUT);
 }
 
 void loop() {
+  leftSensor=digitalRead(5);
+  forwardSensor=digitalRead(6);
+  rightSensor=digitalRead(2);
+  Serial.println(rightSensor);
   maze[x][y]=1;
   if(yon==0){
-    if(rightSensor==1){
+    if(rightSensor==1 && maze[x][y+1]==0){
+      right();
+      forward();
+    }
+    else if(forwardSensor==1 && maze[x-1][y]==0){
+      forward();
+    }
+    else if(leftSensor==1 && maze[x][y-1]==0){
+      left();
+      forward();
+    }
+    else if(maze[x+1][y]==0){
+      back();
+    }
+    else if(rightSensor==1){
       right();
       forward();
     }
@@ -67,14 +90,38 @@ void loop() {
       left();
       forward();
     }
+    else{
+      back();
+    }
   }
   else if(yon==1){
-    if(forwardSensor==1){
+    if(forwardSensor==1 && maze[x][y+1]==0){
+      forward();
+    }
+    else if(leftSensor && maze[x-1][y]==0){
+      left();
+      forward();
+    }
+    else if(rightSensor && maze[x+1][y]==0){
+      right();
+      forward();
+    }
+    else if(maze[x][y-1]==0){
+      back();
+    }
+    else if(forwardSensor==1){
       forward();
     }
     else if(leftSensor){
       left();
       forward();
+    }
+    else if(rightSensor){
+      right();
+      forward();
+    }
+    else{
+      back();
     }
   }
 }
