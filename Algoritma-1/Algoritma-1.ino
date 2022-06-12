90e5ab8b888ce06951cca05d1d583f857779824b
AgentEgg <zenginkagan1@gmail.com>
2022-06-12 20:13:55 +0300

    push

--- Algoritma-1/Algoritma-1.ino
+++ Algoritma-1/Algoritma-1.ino
@@ -1,13 +1,23 @@
+#define fSensor 6 //ön sensör
+#define lSensor 5 //sol sensör
+#define rSensor 4 //sağ sensör
+#define rbSensor 3 //sağ arka sensör
+#define lbSensor 2 //sol arka sensör
+
 //0=left 1=forward 2=right 3=backward
+
 int yon=0;
 int x=9;
 int y=0;
-//ön 6 sol 5 sağ 4 arka sağ 3 arka sol 2 
+
+//ön 6 sol 5 sağ 4 arka sağ 3 arka sol 2
 int maze[ 17 ][ 14 ] = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0  },  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }};
 
-int leftSensor;
-int rightSensor;
-int forwardSensor;
+int lSensor;
+int rSensor;
+int fSensor;
+int lbSensor;
+int rbSensor;
 
 void left (){
   if(yon==0){
@@ -58,33 +68,35 @@ void setup() {
 }
 
 void loop() {
-  leftSensor=digitalRead(5);
-  forwardSensor=digitalRead(6);
-  rightSensor=digitalRead(2);
+  lSensor = digitalRead(lSensor);
+  fSensor = digitalRead(fSensor);
+  rSensor = digitalRead(rSensor);
+  lbSensor = digitalRead(lbSensor);
+  rbSensor = digitalRead(rbSensor);
   maze[x][y]=1;
-  if(yon==0){
-    if(rightSensor==1 && maze[x][y+1]==0){
+  if(yon == 0){
+    if(rSensor == 1 and rbSensor == 1 && maze[x][y+1]==0){
       right();
       forward();
     }
-    else if(forwardSensor==1 && maze[x-1][y]==0){
+    else if(fSensor == 1 && maze[x-1][y]==0){
       forward();
     }
-    else if(leftSensor==1 && maze[x][y-1]==0){
+    else if(lSensor == 1 and lbSensor == 1 && maze[x][y-1] == 0){
       left();
       forward();
     }
-    else if(maze[x+1][y]==0){
+    else if(maze[x+1][y] == 0){
       back();
     }
-    else if(rightSensor==1){
+    else if(rSensor == 1 and rbSensor == 1){
       right();
       forward();
     }
-    else if(forwardSensor==1){
+    else if(fSensor == 1){
       forward();
     }
-    else if(leftSensor==1){
+    else if(lSensor == 1 and lbSensor == 1){
       left();
       forward();
     }
@@ -93,57 +105,57 @@ void loop() {
     }
   }
   else if(yon==1){
-    if(forwardSensor==1 && maze[x][y+1]==0){
+    if(fSensor == 1 && maze[x][y+1] == 0){
       forward();
     }
-    else if(leftSensor && maze[x-1][y]==0){
+    else if(lSensor == 1 and lbSensor == 1 && maze[x-1][y] == 0){
       left();
       forward();
     }
-    else if(rightSensor && maze[x+1][y]==0){
+    else if(rSensor == 1 and rbSensor == 1 && maze[x+1][y] == 0){
       right();
       forward();
     }
-    else if(maze[x][y-1]==0){
+    else if(maze[x][y-1] == 0){
       back();
     }
-    else if(forwardSensor==1){
+    else if(fSensor == 1){
       forward();
     }
-    else if(leftSensor){
+    else if(lSensor == 1 and lbSensor == 1){
       left();
       forward();
     }
-    else if(rightSensor){
+    else if(rSensor == 1 and rbSensor == 1){
       right();
       forward();
     }
     else{
       back();
     }
-  }else if(yon==2){
-    if(leftSensor==1 && maze[x][y+1]){
+  }else if(yon == 2){
+    if(lSensor == 1 and lbSensor == 1 && maze[x][y+1]){
      left();
      forward();
     }
-    else if(forwardSensor==1 && maze[x+1][y]){
+    else if(fSensor == 1 && maze[x+1][y]){
      forward();
     }
-    else if(rightSensor==1 && maze[x][y-1]){
+    else if(rSensor == 1 and rbSensor == 1 && maze[x][y-1]){
       right();
       forward(); 
     }
     else if(maze[x-1][y]==1){
       back();
     }
-    else if(leftSensor==1){
+    else if(lSensor == 1 and lbSensor == 1){
       left();
       forward();
     }
-    else if(forwardSensor==1){
+    else if(fSensor == 1){
       forward();
     }
-    else if(rightSensor==1){
+    else if(rSensor == 1 and rbSensor == 1){
       right();
       forward();
     }
@@ -151,30 +163,30 @@ void loop() {
       back();
     }
   }
-  else if(yon==3){
-    if(leftSensor==1 && maze[x+1][y]){
+  else if(yon == 3){
+    if(lSensor == 1 and lbSensor == 1 && maze[x+1][y]){
      left();
      forward();
     }
-    else if(rightSensor==1 && maze[x-1][y]){
+    else if(rSensor == 1 rbSensor == 1 && maze[x-1][y]){
      forward();
     }
-    else if(forwardSensor==1 && maze[x][y-1]){
+    else if(fSensor == 1 && maze[x][y-1]){
       right();
       forward(); 
     }
     else if(maze[x][y+1]==1){
       back();
     }
-    else if(leftSensor==1){
+    else if(lSensor==1){
       left();
       forward();
     }
-    else if(rightSensor==1){
+    else if(rSensor == 1 and rbSensor == 1){
       right();
       forward();
     }
-    else if(forwardSensor==1){
+    else if(fSensor == 1){
       forward();
     }
     else{
